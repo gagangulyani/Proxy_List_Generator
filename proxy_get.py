ba26196740c7350b8cdd6b5fc04e0a9cd8ef992c
Gagan Gulyani <gagangulyanig@gmail.com>
2020-01-17 16:22:45 +0530

    update: changed return type and updated scrapping, bugs fixed!
    Changed the way content was retrieved; major bugs like Invalid Proxy Info and Weird Output fixed; Now Compatible with Python 3.8.1

--- proxy_get.py
+++ proxy_get.py
@@ -5,15 +5,24 @@ list of proxies along with their port numbers,
 
 try:
     import requests
-    # for getting html of free-proxy-list.net
-except ImportError:
-    print("\n\"requests\" is required for getting proxy list")
-try:
     from bs4 import BeautifulSoup as bs
-    # for scrapping list from html
 except ImportError:
-    print("\n\"bs4\" is required for getting proxy list")
     exit()
+
+
+def p_format(ip_address, port, code, country,
+             anonymity, google, https, last_checked):
+    return {
+        "ip_address": ip_address,
+        "port": port,
+        "code": code,
+        "country": country,
+        "anonymity": anonymity,
+        "google": google,
+        "https": https,
+        "last_checked": last_checked
+    }
+
 
 def Proxy_List():
     """
@@ -21,39 +30,54 @@ def Proxy_List():
     in case of failure.
     """
 
-    url="https://free-proxy-list.net/"
-    # site which will get us list of proxies..
+    # url of site containing proxies
+    url = "https://free-proxy-list.net/"
 
     try:
-        page=requests.get(url)
         # getting html content of site..
-    except :
-        print ("\nFailed to get Proxy List :(")
-        # returns None if unable to get the html of the page
+        page = requests.get(url)
+
+    except:
+        # returns None if unable to get source code of site
+        print("\nFailed to get Proxy List :(")
         return None
 
-    if page.status_code!=200:
-        print ("\nSomething Went Wrong while Getting Proxy List!\n")
+    if page.status_code != 200:
+        print("\nSomething Went Wrong while Getting Proxy List!\n")
         return None
+
+    soup = bs(page.text, 'html.parser')  # creating soup object
+    proxies = []  # final list of dictionaries each containing proxy info
+
+    table = soup.find('table')
+    tbody = None  # cpntains rows of IPs and Stuff
+
+    if table:
+        tbody = table.tbody
+        
+    if tbody:
+        infos = tbody.find_all('tr')
+        for info in infos:
+            # each info is a tr from tbody of table
+            # extracting info from table rows
+            proxy_data_temp = [i.text for i in info]
+            if len(proxy_data_temp) == 8:
+                # after all attributes have been retrieved
+                # from a row, it's time to format it properly.
+                proxies.append(p_format(ip_address=proxy_data_temp[0],
+                                        port=proxy_data_temp[1],
+                                        code=proxy_data_temp[2],
+                                        country=proxy_data_temp[3],
+                                        anonymity=proxy_data_temp[4],
+                                        google=proxy_data_temp[5],
+                                        https=proxy_data_temp[6],
+                                        last_checked=proxy_data_temp[7]))
     
-    soup = bs(page.text,'html.parser') #creating soup object 
-    c=1 # for skipping few irrelevant stuff
-    proxies=[] #the list which will be returned
-    
-    l=[]
-    
-    for i in soup.find_all('tr'):
-        for j in i:
-            if j.name=="td" and ("." in j.text or j.text.isdigit()): # the secret sauce of finding ips
-                #print (j.text)
-                l.append(j.text)
-                if c%2==0:
-                    proxies.append(l)
-                    l=[]
-                c+=1
-    
-    return proxies
+        return {"list_of_proxies": proxies,
+                "count": len(proxies)}
 
 
-if __name__=='__main__':
-    print(Proxy_List())
+if __name__ == '__main__':
+    result = Proxy_List()
+    if result:
+        print(result)
