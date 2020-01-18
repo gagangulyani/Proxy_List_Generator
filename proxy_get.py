ee148e079afc03a4b87732b4b33805aba18b913d
Gagan Deep Singh <gagangulyanig@gmail.com>
2020-01-18 16:27:16 +0530

    update: corrected typos, changed return type, removed unnecessary code in proxy_get.py

--- proxy_get.py
+++ proxy_get.py
@@ -1,6 +1,6 @@
 """
 This script scraps "https://free-proxy-list.net/" and returns
-list of proxies along with their port numbers,
+dictionary containing of proxies along with their port numbers,
 """
 
 try:
@@ -10,8 +10,9 @@ except ImportError:
     exit()
 
 
-def p_format(ip_address, port, code, country,
-             anonymity, google, https, last_checked):
+def p_format(*args):
+    ip_address, port, code, country, anonymity, google, https, last_checked = args
+    
     return {
         "ip_address": ip_address,
         "port": port,
@@ -26,13 +27,13 @@ def p_format(ip_address, port, code, country,
 
 def Proxy_List():
     """
-    This function returns list of proxies abd returns None
+    This function returns list of proxies and returns None
     in case of failure.
     """
 
     # url of site containing proxies
     url = "https://free-proxy-list.net/"
-
+    NUMBER_OF_ATTRIBUTES = 8
     try:
         # getting html content of site..
         page = requests.get(url)
@@ -50,29 +51,19 @@ def Proxy_List():
     proxies = []  # final list of dictionaries each containing proxy info
 
     table = soup.find('table')
-    tbody = None  # cpntains rows of IPs and Stuff
+    tbody = table.tbody if table else None  # contains rows of IPs and Stuff
 
-    if table:
-        tbody = table.tbody
-        
     if tbody:
         infos = tbody.find_all('tr')
         for info in infos:
             # each info is a tr from tbody of table
             # extracting info from table rows
             proxy_data_temp = [i.text for i in info]
-            if len(proxy_data_temp) == 8:
+            if len(proxy_data_temp) == NUMBER_OF_ATTRIBUTES:
                 # after all attributes have been retrieved
                 # from a row, it's time to format it properly.
-                proxies.append(p_format(ip_address=proxy_data_temp[0],
-                                        port=proxy_data_temp[1],
-                                        code=proxy_data_temp[2],
-                                        country=proxy_data_temp[3],
-                                        anonymity=proxy_data_temp[4],
-                                        google=proxy_data_temp[5],
-                                        https=proxy_data_temp[6],
-                                        last_checked=proxy_data_temp[7]))
-    
+                proxies.append(p_format(*proxy_data_temp))
+
         return {"list_of_proxies": proxies,
                 "count": len(proxies)}
 
